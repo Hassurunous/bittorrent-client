2f43e151c24fb5f6f1c70871875ef7eab4a89a92
Michael Loubier <hassurunous@gmail.com>
2024-12-01 21:59:50 -0800

    Build function to create formatted strings for encoding. Initially tried to convert directly to byte array, but actually need to combine all items to be encoded before converting to byte array. Left commented methods for now just in case they prove useful later.

--- bittorrent-client/_Scripts/BEncoding/BEncoding.cs
+++ bittorrent-client/_Scripts/BEncoding/BEncoding.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Bittorrent
 {
@@ -21,19 +22,25 @@ namespace Bittorrent
         //  Negative integers are allowed.
         //  i-0e is invalid (use i0e)
         //  Leading zeros are invalid (except for the case of i0e to represent the number 0)
-        private static byte IntegerStart = Encoding.UTF8.GetBytes("i")[0];      //105
-        private static byte IntegerEnd = Encoding.UTF8.GetBytes("e")[0];        //101
+        private static readonly byte IntegerStart = Encoding.UTF8.GetBytes("i")[0];      //105
+        private static readonly byte IntegerEnd = Encoding.UTF8.GetBytes("e")[0];        //101
 
         //List Format: lXXXXe where XXXX are the (also bencoded) elements of the list
         //EXAMPLES:
         //  Example: l4:milk4:eggs5:bread13:energy drinks
         //  Example: li17ei12ei13e
-        private static byte ListStart = Encoding.UTF8.GetBytes("l")[0];         //108
-        private static byte ListEnd = Encoding.UTF8.GetBytes("e")[0];           //101
+        private static readonly byte ListStart = Encoding.UTF8.GetBytes("l")[0];         //108
+        private static readonly byte ListEnd = Encoding.UTF8.GetBytes("e")[0];           //101
 
         //Dictionary Format: dWWWXXXYYYZZZe where WWW+XXX and YYY+ZZZ are the key-value pairs to store 
-        private static byte DictionaryStart = Encoding.UTF8.GetBytes("d")[0];   //100
-        private static byte DictionaryEnd = Encoding.UTF8.GetBytes("e")[0];     //101
+        //NOTES:
+        //  Keys MUST be strings
+        private static readonly byte DictionaryStart = Encoding.UTF8.GetBytes("d")[0];   //100
+        private static readonly byte DictionaryEnd = Encoding.UTF8.GetBytes("e")[0];     //101
+
+        //Separator: Separates prefix from encoded values
+        private static readonly byte[] PrefixSeparator = Encoding.UTF8.GetBytes(":");     //58
+        private static readonly char PrefixSeparatorChar = ":"[0];
 
 
         //Helper. Throw functions in here that we want to test in the console.
@@ -46,7 +53,63 @@ namespace Bittorrent
             Console.WriteLine("ListEnd: {0}", ListEnd);
             Console.WriteLine("DictionaryStart: {0}", DictionaryStart);
             Console.WriteLine("DictionaryEnd: {0}", DictionaryEnd);
+            Console.WriteLine("PrefixDivider: {0}", PrefixSeparator);
             Console.WriteLine("stringToCount Length: {0}", Encoding.UTF8.GetBytes(stringToCount).Length);
         }
+
+        //Encode values to expected bencoded string
+        private static string Encode(byte[] bytes)
+        {
+            return "";
+        }
+
+        //Encode string value
+        public static string FormatStringItem(string str) 
+        {
+            if (string.IsNullOrEmpty(str)) return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Encoding.UTF8.GetByteCount(str));
+            sb.Append(PrefixSeparatorChar);
+            sb.Append(str);
+
+            return sb.ToString();
+        }
+
+        private static byte[] ByteArrayBuilder(byte[][] arrays)
+        {
+            byte[] ret = new byte[arrays.Sum(x => x.Length)];
+            int offset = 0;
+            foreach (byte[] data in arrays)
+            {
+                Buffer.BlockCopy(data, 0, ret, offset, data.Length);
+                offset += data.Length;
+            }
+            return ret;
+        }
+
+        //public static byte[] BuildDataByteArray(byte[] prefix, byte[] data, byte[]? suffix = null)
+        //{
+        //    try 
+        //    {
+        //        //Prefix separator is always the same, so we don't need to pass that in.
+        //        //Suffix isn't used for strings
+        //        byte[][] paramsToPass = suffix != null ? [prefix, data, suffix] : [prefix, data];
+        //        return ByteArrayBuilder(paramsToPass);
+        //    }
+        //    catch (Exception e) 
+        //    {
+        //        throw e;
+        //    }
+        //}
+
+        //public static byte[] BuildDataByteArrayForStringData(string str)
+        //{
+        //    //Strings should be preceded by a byte-count of the string length
+        //    byte[] stringLength = [Convert.ToByte(GetStringByteLength(str))];
+        //    byte[] bytes = Encoding.UTF8.GetBytes(str);
+
+        //    return BuildDataByteArray(stringLength, bytes);
+        //}
     }
 }
---
+++ bittorrent-clientTests/_Scripts/BEncoding/BEncodingTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bittorrent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitTorrentTests
+{
+    [TestClass]
+    public class BEncodingTests
+    {
+        #region Encoding/Decoding Tests
+
+        #region Test Values
+        private readonly Dictionary<string, string> EncodingStringsTestValues = new Dictionary<string, string>()
+        {
+            {"test", "4:test" },
+            {"strings with spaces", "19:strings with spaces" },
+            {"""punctuation check!@#$%^&*()_+-=,./;'[]\<>?:"{}|""", """47:punctuation check!@#$%^&*()_+-=,./;'[]\<>?:"{}|"""},
+            {"iwonderhowlongthestringsyoucanencodeare?istherealimit?", "54:iwonderhowlongthestringsyoucanencodeare?istherealimit?" },
+            {"""multi-byte characters 𐀏𐀏𐀀𐀀""",  """38:multi-byte characters 𐀏𐀏𐀀𐀀"""},
+            {"", "" }
+        };
+
+        private readonly Dictionary<string, string> EncodingIntegersTestValues = new Dictionary<string, string>()
+        {
+
+        };
+
+        private readonly Dictionary<string, string> EncodingListsTestValues = new Dictionary<string, string>()
+        {
+
+        };
+
+        private readonly Dictionary<string, string> EncodingDictionaryStringIntTestValues = new Dictionary<string, string>()
+        {
+        };
+
+        private readonly Dictionary<string, string> EncodingDictionaryStringListTestValues = new Dictionary<string, string>()
+        {
+            { """d4:spaml1:a1:bee {'spam': ['a', 'b']}""", """{'spam': ['a', 'b']}""" }
+        };
+
+        #endregion
+
+        [TestMethod]
+        public void FormatStringItem_Test()
+        {
+            string encodedString;
+            foreach (KeyValuePair<string, string> stringToTest in EncodingStringsTestValues)
+            {
+                Console.WriteLine($"Testing string: {stringToTest.Key}");
+                Console.WriteLine($"Expected Value: {stringToTest.Value}");
+                encodedString = BEncoding.FormatStringItem(stringToTest.Key);
+                Assert.IsNotNull(encodedString);
+                Assert.AreEqual(stringToTest.Value, encodedString);
+            }
+        }
+        #endregion
+    }
+}
